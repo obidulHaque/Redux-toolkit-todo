9469d33ddd702f1de28e3fcb426e016db46524a0
obidulHaque <obaidulhoqejoy@gmail.com>
2024-06-12 15:15:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./Components/Todo";
+import Todos from "./Components/Todos";
+
+function App() {
+  return (
+    <>
+      <Todo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
